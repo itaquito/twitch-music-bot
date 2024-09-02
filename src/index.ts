65a4e039b542da1bc87917b3d879755c31e3a98a
itaquito <ichilaquiles@gmail.com>
2024-09-02 13:56:36 -0600

    feat: Setup webserver

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello world');
+import 'dotenv/config';
+
+import express from 'express';
+
+const app = express();
+
+app.get('*', (req, res) => {
+  res.status(404).send('404 Not found');
+});
+
+app.listen(process.env.AUTH_SEVER_PORT, () => {
+  console.log(
+    `[!] The auth server is running on http://localhost:${process.env.AUTH_SEVER_PORT}`
+  );
+});
